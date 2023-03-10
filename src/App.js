9979aef14249256f145722aa4ba8158a3cc731df
44hukum <hukum.gosain@resimator.fi>
2023-03-11 01:35:29 +0545

    Added initial data loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      viewActive: false,
+      activeItem:{
+        name: "",
+        active: true
+      },
+      users:[]
+    }
+  }
+
+  async componentDidMount(){
+    try{
+      const res = await fetch('http://localhost:8000/api/users')
+      const users = await res.json()
+      console.log(users)
+      this.setState({
+        users
+      });
+    } catch(e){
+      console.log(e)
+    }
+  }
+
+  renderItems = ()=>{
+
+    return this.state.users.map(users => (
+      <li>
+            {users.active}, 
+        {users.name} </li>
+    ));
+  }
+
+  render(){
+    return(
+      <div>
+        {this.renderItems()}
+      </div>
+    )
+  }
 }
 
 export default App;
